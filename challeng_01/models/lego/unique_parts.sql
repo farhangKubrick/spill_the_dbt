834757f69682319fed9a82147b17cfee2a20a1c6
farhangKubrick <114756554+farhangKubrick@users.noreply.github.com>
2025-04-26 15:45:58 +0100

    commiting changes

---
+++ challeng_01/models/lego/unique_parts.sql
@@ -0,0 +1,12 @@
+WITH UNIQUE_PARTS AS (
+SELECT 
+	P.part_num
+FROM parts as P
+INNER JOIN inventory_parts as IP on P.part_num = IP.part_num
+INNER JOIN inventories as I on I.id = IP.inventory_id
+INNER JOIN sets as S on S.set_num = I.set_num
+	GROUP BY P.part_num
+	HAVING COUNT(*) = 1
+)
+
+SELECT * from UNIQUE_PARTS
